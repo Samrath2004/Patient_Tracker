7d0916a5cc57ea65d67d9223d4baa456af709f2b
Samrath2004 <113513345+Samrath2004@users.noreply.github.com>
2023-11-16 10:07:21 +0530

    logger

---
+++ Logger.h
@@ -0,0 +1,28 @@
+#ifndef HOSPITALAPP_LOGGER_H
+#define HOSPITALAPP_LOGGER_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Logger {
+public:
+    static Logger& getInstance();
+    void log(const string& output);
+    void setDebugMode(bool in);
+    bool getDebugMode();
+    ~Logger();
+private:
+    Logger();
+    static bool debugMode;
+    ofstream file;
+};
+
+extern Logger &logger;
+
+
+#endif
+
+
